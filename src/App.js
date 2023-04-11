b50fcb8ed19fc50adb352221811bd32094dce119
Yixiang <yixiangw3@student.unimelb.edu.au>
2023-04-11 23:27:00 +1000

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
 import Scatter from './components/scatter';
+import NavBar from './components/navBar';
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]); 
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#ffffff',
+      },
+    },
+  });
 
   useEffect(() => {
     fetch("data.json")
@@ -14,21 +24,24 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Scatter data={products} />
+      <ThemeProvider theme={theme}>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        <NavBar />
+        <Scatter data={products} />
+      </ThemeProvider>
     </div>
   );
 }
